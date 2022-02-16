ca47623f15ee528cea2bcce8f50e1f3cc4010e71
Adrian Pinto <adripintogonzalez@gmail.com>
2022-02-16 11:29:43 +0100

    add nested routes example

---
+++ lectures/10_react-basics/examples/nested-routes/src/pages/UserHobbies.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserHobbies = () => (
+  <p>User Hobbies</p>
+);
+
+export default UserHobbies;
---
+++ lectures/10_react-basics/examples/nested-routes/src/pages/UserOptions.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserOptions = () => (
+  <p>Settings Page</p>
+);
+
+export default UserOptions;
